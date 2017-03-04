9d04be0b9dcdf4d9867cc7440b211049efe279b1
Jieun Rim <rim.jieun@gmail.com>
2017-03-04 13:51:41 -0500

    js added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
 
 // When user clicks 
+$("#search-button").on("click", function() {
 
+	var query = $("#formGroup").val();
+	var page = $("#numRecords").val();
+	var beginDate = $("#startYear").val();
+	var endDate = $("#endYear").val();
+
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json/";
+	url += '?' + $.param({
+	  'api-key': "de0ba28506cb461fa1eb236d6d03c3e2",
+	  'q': query,
+	  'page': page,
+	  'begin_date': beginDate,
+	  'end_date': endDate
+	});
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(result) {
+
+		console.log(result);
+
+	}).fail(function(err) {
+
+		  throw err;
+	});
+
+
+})
 
 
 
