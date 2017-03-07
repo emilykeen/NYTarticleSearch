dd5921caa04532347b891fabb32761a996b4dc21
Jieun Rim <rim.jieun@gmail.com>
2017-03-07 17:04:02 -0500

    changes made to all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,107 @@
 $(document).ready(function() {
 
-// When user clicks 
-$("#submitBtn").on("click", function() {
+	var query = "";
+	var startYear = 0;
+	var endYear = 0;
+	var numResults = 0;
 
-	var query = $("#formGroup").val();
-	var page = $("#numRecords").val();
-	var beginDate = $("#startYear").val();
-	var endDate = $("#endYear").val();
+	var baseURL = "http://developer.nytimes.com/proxy/https/api.nytimes.com/svc/search/v2/articlesearch.json";
 
-	var url = "http://developer.nytimes.com/proxy/https/api.nytimes.com/svc/search/v2/articlesearch.json";
-	url += '?' + $.param({
-	  'api_key': "de0ba28506cb461fa1eb236d6d03c3e2",
-	  'q': query,
-	  'page': page,
-	  'begin_date': beginDate,
-	  'end_date': endDate
-	});
+	function runQuery(numArticles, queryURL) {
 
-	console.log(url);
+		$.ajax({
+		  url: queryURL,
+		  method: "GET"
+		}).done(function(results) {
 
-	$.ajax({
-	  url: url,
-	  method: 'GET'
-	}).done(function(result) {
+			console.log(results);
+			
 
-		console.log(result);
+			for (i = 0; i < numArticles; i++) {
 
-		console.log(result.docs[])
+				var article = results.response.docs[i];
+				var title;
+				var author;
+				var date;
+				var webURL;
 
-	}).fail(function(err) {
+				if (article.headline !== null) {
+					title = article.headline.main.split(";");
+					title = title[0];
+				}
+				else {
+					title = null;
+				}
+				if (article.byline !== null) {
+					author = article.byline.original;
+				}
+				else {
+					author = null;
+				}
+				if (article.pub_date !== null) {
+					date = article.pub_date.split("T");
+					date = date[0];
+				}
+				if (article.web_url !== null) {
+					webURL = article.web_url;
+				}
 
-		  throw err;
-	});
+				console.log(title);
+				console.log(author);
+				console.log(date);
+				console.log(webURL);
+
+				var newDiv = $("<div class='resultDiv'>");
+				var headingSec = $("<h3>" + title + "</h3>");
+				var authorSec = $("<p>" + author + "</p>");
+				var dateSec = $("<p>" + date + "</p>");
+				var urlSec = $("<a href=" + webURL + " " + "target='_blank_'>" + webURL + "</p>");
+				
+				newDiv.append(headingSec)
+				.append(authorSec)
+				.append(dateSec)
+				.append(urlSec);
+
+				$("#searchRes").append(newDiv);
+				
+			}
+
+		}).fail(function(err) {
+			  throw err;
+		});
+	}
+
+	$("#submitBtn").on("click", function(event) {
+
+		event.preventDefault();
+
+		query = $("#formGroup").val().trim();
+		startYear = $("#startYear").val().trim();
+		endYear = $("#endYear").val().trim();
+		numResults = $("#numRecords option:selected").val().trim();
+
+		console.log(numResults);
+
+		var newURL = baseURL + '?' + $.param({
+		  "api_key": "de0ba28506cb461fa1eb236d6d03c3e2",
+		  "q": query,
+		});
+
+		if (parseInt(startYear)) {
+				startYear += "0101";
+				newURL = newURL + "&begin_date=" + startYear;
+		}
+		if (parseInt(endYear)) {
+				endYear += "0101";
+				newURL = newURL + "&end_date=" + endYear;
+		}
+
+		runQuery(numResults, newURL);
+
+		
 
 
-})
+	})
 
 
 
